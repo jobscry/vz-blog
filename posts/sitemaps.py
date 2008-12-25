bb44b5f16f7390ef615e6a196e1c192fce959326
Joe Vasquez <joe.vasquez@gmail.com>
2008-12-25 14:52:01 -0500

    added sitemaps and flatpages.  added page-title block to all templates.

---
+++ posts/sitemaps.py
@@ -0,0 +1,15 @@
+from django.contrib.sitemaps import Sitemap
+from models import Post
+
+class BlogSitemap(Sitemap):
+    changefreq = "never"
+    priority = 0.6
+    
+    def items(self):
+        return Post.objects.filter(is_published=True)
+
+    def lastmod(self, obj):
+        return obj.published_on
+
+    def location(self, obj):
+        return obj.get_absolute_url()
--- urls.py
+++ urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls.defaults import *
+from django.contrib.sitemaps import GenericSitemap
+from blog.posts.sitemaps import BlogSitemap
 
 from django.contrib import admin
 admin.autodiscover()
 
+sitemaps = {
+    'blog': BlogSitemap,
+}
+
 urlpatterns = patterns('',
     #(r'^comments/', include('django.contrib.comments.urls')),
+
+    (r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
 
     (r'^posts/', include('blog.posts.urls')),
 	(r'^users/', include('blog.users.urls')),
