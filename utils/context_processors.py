2e8dcdd6f39a2d97c618e07858866d12d80fd233
Joe Vasquez <joe.vasquez@gmail.com>
2008-12-25 17:19:39 -0500

    added list of flat pages to context processors, now flatpage links in base.html

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -1,8 +1,11 @@
 from django.conf import settings
+from django.contrib.flatpages.models import FlatPage
 from django.contrib.sites.models import Site
 from blog.posts.models import Post
 
 def base_url(request):
     current_site = Site.objects.get(id=settings.SITE_ID)
     return { 'base_url': 'http://%s'%current_site.domain }
-    
+
+def flatpage_list(request):
+    return { 'flatpages': FlatPage.objects.all().order_by('title') }
