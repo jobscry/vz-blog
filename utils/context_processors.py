8a0aa22357217772ec60fafc2381b81d7ca4bd5f
Joe Vasquez <joe.vasquez@gmail.com>
2008-12-29 13:31:40 -0500

    added preview to post list instead of whole post

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -3,6 +3,9 @@ from django.contrib.flatpages.models import FlatPage
 from django.contrib.sites.models import Site
 from blog.posts.models import Post
 
+def preview_length(request):
+    return { 'preview_length': settings.POST_PREVIEW_LENGTH }
+
 def base_url(request):
     current_site = Site.objects.get(id=settings.SITE_ID)
     return { 'base_url': 'http://%s'%current_site.domain }
