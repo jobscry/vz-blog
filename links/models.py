5a456d87ae48e0beda6bbf3f3d72e1febbd377d2
Joe Vasquez <joe.vasquez@gmail.com>
2009-02-14 20:10:39 -0500

    added extra field to links

--- links/models.py
+++ links/models.py
@@ -8,8 +8,9 @@ class Link(models.Model):
     
     Link model for linkroll
     """
-    title = models.CharField(max_length='255')
+    title = models.CharField(max_length=255)
     url = models.URLField(verify_exists=False)
+    extra = models.CharField(max_length=100, default='', blank=True, null=True)
     tags = TagField()
     added_by = models.ForeignKey(User)
     changed_on = models.DateTimeField(blank=True, null=True)
