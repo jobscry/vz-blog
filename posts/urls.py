2dcda1ea60d9a1ed66f2113d1bedf342babbe71a
Joe Vasquez <joe.vasquez@gmail.com>
2009-02-26 11:53:35 -0500

    fixed wrong variable name for pagination in posts_list

--- posts/urls.py
+++ posts/urls.py
@@ -18,7 +18,7 @@ urlpatterns += patterns('posts.views',
 	url(r'archive/(?P<year>\d{4})/(?P<month>\d{1,2})$', 'archive', name="archive_month"),
 	url(r'search/$', 'search_posts', name="search_posts"),
 	url(r'tags/$', 'posts_by_tag', name="posts_by_tag"),
-    url(r'page(?P<page>[0-9]+)/$', 'posts_list', name="posts_list_page"),
+    url(r'page(?P<page_num>[0-9]+)/$', 'posts_list', name="posts_list_page"),
 	url(r'(?P<slug>[\w\-]+)/$', 'view_post', name="view_post"),
     url(r'$', 'posts_list', { 'page_num': 1, }, name="posts_list"),
 )
