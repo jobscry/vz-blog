58a2af5be0f57ccb987bb1e7f6e805259bc2692d
Joe Vasquez <joe.vasquez@gmail.com>
2009-03-21 20:45:04 -0400

    updated to jquery 1.3.2, updated stream icons, removed stream link on base template, added ajax stream in base template, added dependency to simplejson

--- stream/urls.py
+++ stream/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('stream.views',
+    url(r'ajax/$', 'view_stream', { 'pk': 1, }),
     url(r'$', 'view_stream', { 'pk': 1, }, name="view_stream"),
 )
--- stream/views.py
+++ stream/views.py
@@ -1,12 +1,31 @@
+from django.http import HttpResponse
 from django.shortcuts import get_list_or_404, get_object_or_404
-from models import Stream, Entry
+from django.utils.encoding import force_unicode
+from models import Stream, Entry, RssFeed
 from utils import render_to_response
 
 def view_stream(request, pk):
     stream = get_object_or_404(Stream, pk=pk)
+    entries = Entry.objects.filter(stream=stream)[:25]
+    if request.is_ajax():
+        import simplejson as json
+        data = list()
+        for entry in entries:
+            data.append({
+                'feed': entry.feed.title,
+                'title': entry.title,
+                'link': entry.link,
+                'published_on': entry.published_on.isoformat()
+            })
+        response = HttpResponse(mimetype='application/json')
+        response.write(
+            json.dumps(data)
+        )
+        return response
+
     return render_to_response(
         'view-stream.html',
-        { 'entries': get_list_or_404(Entry, stream=stream) },
+        { 'entries':  entries },
         request
     )
 
