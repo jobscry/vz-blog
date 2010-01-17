5713c659860da8a4e0bff94c46b66e3d6b5f0ea7
Joe Vasquez <joe.vasquez@gmail.com>
2010-01-16 23:24:50 -0500

    added caching to context_processors

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -2,26 +2,39 @@
 from django.conf import settings
 from django.contrib.flatpages.models import FlatPage
 from django.contrib.sites.models import Site
+from django.core.cache import cache
 from tagging.models import Tag
 from tagging.utils import LOGARITHMIC
 from blog.posts.models import Post
 from blog.links.models import Link
 
 def blog_info(request):
+    linkroll = cache.get('blog_linkroll', None)
+    if linkroll is None:
+        cache.set('blog_linkroll',  Link.objects.all(), 60*15)
+        linkroll = cache.get('blog_linkroll')
+
+    tags = cache.get('blog_tags', None)
+    if tags is None:
+        cache.set('blog_tags',  Tag.objects.cloud_for_model(Post, steps=10, min_count=1, distribution=LOGARITHMIC), 60*15)
+        tags = cache.get('blog_tags')
     return { 
         'blog_tagline': settings.BLOG_TAGLINE,
         'blog_title': settings.BLOG_TITLE,
         'blog_copyright': settings.BLOG_COPYRIGHT,
         'blog_copyright_url': settings.BLOG_COPYRIGHT_URL,
         'blog_preview_length': settings.POST_PREVIEW_LENGTH,
-        'blog_linkroll': Link.objects.all(),
-        'blog_tags': Tag.objects.cloud_for_model(Post, steps=10, min_count=1, distribution=LOGARITHMIC),
+        'blog_linkroll': linkroll,
+        'blog_tags': tags,
         'blog_feedburner_url': settings.FEEDBURNER_URL,
     }
 
 def base_url(request):
-    current_site = Site.objects.get(id=settings.SITE_ID)
-    return { 'base_url': 'http://%s'%current_site.domain }
+    site = cache.get('current_site', None)
+    if site is None:
+        cache.set('current_site', Site.objects.get(id=settings.SITE_ID).domain, 60*15)
+        cache.get('current_site')
+    return { 'base_url': 'http://%s'%site }
 
 def extra_meta(request):
     return { 'extra_meta': settings.EXTRA_META }
@@ -30,7 +43,11 @@ def google_analytics_js(request):
     return { 'google_analytics_js': settings.GOOGLE_ANALYTICS_JS }
 
 def flatpage_list(request):
-    return { 'flatpages': FlatPage.objects.all().order_by('title') }
+    fpages = cache.get('flatpages', None)
+    if fpages is None:
+        cache.set('flatpages', FlatPage.objects.all().order_by('title'), 60*15)
+        fpages = cache.get('flatpages')
+    return { 'flatpages': fpages }
 
 def disqus(request):
     if settings.DISQUS:
