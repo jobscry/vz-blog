af558b715dd206e5447d5771333f887c73008278
Joe Vasquez <joe.vasquez@gmail.com>
2010-04-12 17:36:02 -0400

    switched to django cached template loader

--- default.settings.py
+++ default.settings.py
@@ -56,9 +56,10 @@ SECRET_KEY = ''
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
 )
 
 MIDDLEWARE_CLASSES = (
