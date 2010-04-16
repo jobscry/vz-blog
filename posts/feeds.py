14e7f7010a005fb645e7f62187a0ac85c6089ac3
Joe Vasquez <joe.vasquez@gmail.com>
2010-04-16 01:19:55 -0400

    refactored feeds

--- posts/feeds.py
+++ posts/feeds.py
@@ -3,13 +3,9 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 from django.contrib.syndication.feeds import Feed
-from django.utils.feedgenerator import Atom1Feed
-from tagging.models import Tag, TaggedItem
 from models import Post
 
 class LatestPosts(Feed):
-    title_template = 'feeds/default-title.html'
-    description_template = 'feeds/default-description.html'
     
     def title(self):
         return settings.BLOG_TITLE
@@ -31,22 +27,14 @@ class LatestPosts(Feed):
         current_site = Site.objects.get(id=settings.SITE_ID)
         return 'http://%s/'%current_site.domain
 
-    def categories(self):
-        tags = Tag.objects.usage_for_model(Post, counts=False, min_count=None, filters=None)
-        tags_list = []
-        for tag in tags:
-            tags_list.append(tag.name)
-        return tags_list
-
     def copyright(self):
         return settings.BLOG_COPYRIGHT
 
     def items(self):
         return Post.objects.filter(is_published=True).order_by('-published_on')[:10]
 
-    def item_link(self, item):
-        current_site = Site.objects.get(id=settings.SITE_ID)
-        return 'http://%s%s'%(current_site.domain, item.get_absolute_url())
+    def item_description(self, item):
+        return item.body
 
     def item_author_name(self, item):
         return item.author.get_full_name()
@@ -58,11 +46,7 @@ class LatestPosts(Feed):
         return item.published_on
 
     def item_categories(self, item):
-        tags = Tag.objects.get_for_object(item)
-        tags_list = []
-        for tag in tags:
-            tags_list.append(tag.name)
-        return tags_list
+        return item.tags.split(' ')
 
     def item_copyright(self):
         return settings.BLOG_COPYRIGHT
