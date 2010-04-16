e22a5d8a08de351aebe6f734b0cf89663b7ea1ca
Joe Vasquez <joe.vasquez@gmail.com>
2010-04-16 01:26:55 -0400

    upped version number

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 # -*- mode: python; coding: utf-8; -*-
-VERSION = (1, 3, 0)
+VERSION = (1, 3, 1)
 __version__ = '.'.join(map(str, VERSION))
 __author__ = 'Joe Vasquez'
 __email__ = 'joe.vasquez@gmail.com'
