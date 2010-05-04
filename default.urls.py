b767499273f413fbcb3de50a853908cc4a3eb3c2
Joe Vasquez <joe.vasquez@gmail.com>
2010-05-04 03:47:53 -0400

    added cache status check

--- default.urls.py
+++ default.urls.py
@@ -28,4 +28,6 @@ urlpatterns = patterns('',
 
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'users/login.html' }, name='login'),
 	url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
+	
+	(r'^status/cache/$', 'utils.memcached_status.view'),
 )
